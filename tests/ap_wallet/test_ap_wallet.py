eb7fe758552a7c47c5bebbd9bf5aaed0b7477ddc
Matthew Howard <matt@chia.net>
2020-05-28 14:42:01 +0100

    added another test for when signature is not present

--- tests/ap_wallet/test_ap_wallet.py
+++ tests/ap_wallet/test_ap_wallet.py
@@ -164,6 +164,7 @@ class TestWalletSimulator:
         await self.time_out_assert(15, wallet2.get_confirmed_balance, 20)
         await self.time_out_assert(15, wallet2.get_unconfirmed_balance, 20)
 
+
     @pytest.mark.asyncio
     async def test_siphon_value_from_spend(self, two_wallet_nodes):
         num_blocks = 10
@@ -401,3 +402,106 @@ class TestWalletSimulator:
         await self.time_out_assert(15, ap_wallet.get_unconfirmed_balance, 5)
         await self.time_out_assert(15, wallet2.get_confirmed_balance, 170)
         await self.time_out_assert(15, wallet2.get_unconfirmed_balance, 170)
+
+    @pytest.mark.asyncio
+    async def test_ap_spend_invalid(self, two_wallet_nodes):
+        num_blocks = 10
+        full_nodes, wallets = two_wallet_nodes
+        full_node_1, server_1 = full_nodes[0]
+        wallet_node, server_2 = wallets[0]
+        wallet_node_2, server_3 = wallets[1]
+        wallet = wallet_node.wallet_state_manager.main_wallet
+        wallet2 = wallet_node_2.wallet_state_manager.main_wallet
+
+        ph = await wallet.get_new_puzzlehash()
+
+        await server_2.start_client(PeerInfo("localhost", uint16(server_1._port)), None)
+        await server_3.start_client(PeerInfo("localhost", uint16(server_1._port)), None)
+
+        for i in range(1, num_blocks):
+            await full_node_1.farm_new_block(FarmNewBlockProtocol(ph))
+
+        funds = sum(
+            [
+                calculate_base_fee(uint32(i)) + calculate_block_reward(uint32(i))
+                for i in range(1, num_blocks - 2)
+            ]
+        )
+
+        await self.time_out_assert(15, wallet.get_confirmed_balance, funds)
+
+        # Get pubkeys for creating the puzzle
+        devrec = await wallet.wallet_state_manager.get_unused_derivation_record(
+            wallet.wallet_info.id
+        )
+        ap_pubkey_a = devrec.pubkey
+        ap_wallet: APWallet = await APWallet.create_wallet_for_ap(
+            wallet_node_2.wallet_state_manager, wallet2, ap_pubkey_a
+        )
+        ap_pubkey_b = ap_wallet.ap_info.my_pubkey
+
+        ap_puz = ap_puzzles.ap_make_puzzle(ap_pubkey_a, ap_pubkey_b)
+        sig = await wallet.sign(ap_puz.get_tree_hash(), bytes(ap_pubkey_a))
+        assert sig is not None
+        await ap_wallet.set_sender_values(ap_pubkey_a, sig)
+        assert ap_wallet.ap_info.change_signature is not None
+        assert BLSSignature.from_bytes(ap_wallet.ap_info.change_signature).validate(
+            [BLSSignature.PkMessagePair(ap_pubkey_a, ap_puz.get_tree_hash())]
+        )
+        assert BLSSignature.from_bytes(ap_wallet.ap_info.change_signature).validate(
+            [
+                BLSSignature.PkMessagePair(
+                    PublicKey.from_bytes(ap_wallet.ap_info.authoriser_pubkey),
+                    ap_puz.get_tree_hash(),
+                )
+            ]
+        )
+        tx = await wallet.generate_signed_transaction(100, ap_puz.get_tree_hash())
+        await wallet.push_transaction(tx)
+
+        for i in range(1, num_blocks):
+            await full_node_1.farm_new_block(FarmNewBlockProtocol(ph))
+
+        await self.time_out_assert(15, ap_wallet.get_confirmed_balance, 100)
+        await self.time_out_assert(15, ap_wallet.get_unconfirmed_balance, 100)
+
+        ph2 = await wallet2.get_new_puzzlehash()
+
+        amount = 100
+        puzzlehash = ph2
+        # calculate amount of transaction and change
+        coins = await ap_wallet.select_coins(amount)
+        assert coins is not None
+        assert coins is not set()
+        spends = []
+        coin = coins.pop()
+        solution = ap_puzzles.ap_make_solution(
+            [(puzzlehash, amount)], coin.parent_coin_info, ap_puz.get_tree_hash()
+        )
+        spends.append((ap_puz, CoinSolution(coin, solution)))
+
+        tx = await ap_wallet.ap_sign_transaction(spends, [])
+
+        assert tx is not None
+
+        tx_record = TransactionRecord(
+            confirmed_at_index=uint32(0),
+            created_at_time=uint64(int(time.time())),
+            to_puzzle_hash=ph,
+            amount=uint64(amount),
+            fee_amount=uint64(0),
+            incoming=False,
+            confirmed=False,
+            sent=uint32(0),
+            spend_bundle=tx,
+            additions=tx.additions(),
+            removals=tx.removals(),
+            wallet_id=ap_wallet.wallet_info.id,
+            sent_to=[],
+        )
+        await ap_wallet.wallet_state_manager.add_pending_transaction(tx_record)
+        for i in range(1, num_blocks):
+            await full_node_1.farm_new_block(FarmNewBlockProtocol(ph))
+
+        await self.time_out_assert(15, ap_wallet.get_confirmed_balance, 100)
+        await self.time_out_assert(15, ap_wallet.get_unconfirmed_balance, 0)
