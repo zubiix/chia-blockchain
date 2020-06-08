0b5f8b83b57944e3d61a266967df49cdefd4253e
Richard Kiss <him@richardkiss.com>
2020-06-08 13:59:54 -0700

    Add `chia version -r` option.

--- src/cmds/version.py
+++ src/cmds/version.py
@@ -2,9 +2,19 @@ from src import __version__
 
 
 def version(args, parser):
-    print(__version__)
+    if args.root:
+        print(args.root_path)
+    else:
+        print(__version__)
 
 
 def make_parser(parser):
+    parser.add_argument(
+        "-r",
+        "--root",
+        action="store_true",
+        help="Display root directory",
+    )
+
     parser.set_defaults(function=version)
     return parser
